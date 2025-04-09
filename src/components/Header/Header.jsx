c2c76af4da59f754ae6d643e245b8a4a381b9a33
Rohit2133 <144320192+Rohit2133@users.noreply.github.com>
2025-04-09 23:07:55 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your Favourite food here.</h2>
+            <p>Food that fuels your day and flavors that lift your mood.
+            Order fresh, tasty meals from trusted kitchens, made with care and delivered with love.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+
+
+const navbar = () => {
+    const [menu,setMenu] = useState("home")
+  return (
+    <div className='nav'>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="nav-menu">
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
+      </ul>
+      <div className="nav-right">
+        <img src={assets.search_icon} alt="" className="search" />
+        <div className="nav-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  )
+}
+
+export default navbar

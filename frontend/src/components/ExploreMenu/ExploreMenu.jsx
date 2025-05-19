2d97a117c8b687e0d1123a363a1073bc9270a7a4
Aayush Kansal <155978676+aayushkansal79@users.noreply.github.com>
2025-05-19 15:06:06 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,7 +4,7 @@ import { menu_list } from '../../assets/assets'
 const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='exp-menu' id='exp-menu'>
-        <h1>Explore our Menu</h1>
+        <h1>Explore our Menu..</h1>
         <p className='exp-menu-text'>Every dish tells a story.
         From appetizers to desserts, our menu brings together fresh ingredients, authentic recipes, and mouthwatering flavors you'll come back for.</p>
         <div className="exp-menu-list">
